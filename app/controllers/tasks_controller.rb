f398986183aba290d73a56fe3f095f2bca36b928
rafaelsales03 <elrafael76234@gmail.com>
2025-10-08 17:51:34 -0300

    change in tasks_controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,27 +1,24 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: %i[ show edit update destroy ]
-
+  before_action :set_task, only: %i[show edit update destroy]
 
   def index
     @tasks = Task.order(completed: :asc, due_date: :asc)
   end
 
-  def show
-  end
+  def show; end
 
   def new
     @task = Task.new
   end
 
-  def edit
-  end
+  def edit; end
 
   def create
     @task = Task.new(task_params)
 
     respond_to do |format|
       if @task.save
-        format.html { redirect_to tasks_path, notice: "Task was successfully created." }
+        format.html { redirect_to tasks_path, notice: 'Task was successfully created.' }
         format.json { render :show, status: :created, location: @task }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -33,7 +30,7 @@ class TasksController < ApplicationController
   def update
     respond_to do |format|
       if @task.update(task_params)
-        format.html { redirect_to tasks_path, notice: "Task was successfully updated." }
+        format.html { redirect_to tasks_path, notice: 'Task was successfully updated.' }
         format.json { render :show, status: :ok, location: @task }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -46,34 +43,34 @@ class TasksController < ApplicationController
     @task = Task.find(params[:id])
 
     respond_to do |format|
-    if @task.update(completed: true)
-        format.html { redirect_to tasks_path, notice: "Task was successfully completed." }
+      if @task.update(completed: true)
+        format.html { redirect_to tasks_path, notice: 'Task was successfully completed.' }
         format.json { head :ok }
         format.js
-    else
-        format.html { redirect_to tasks_path, alert: "Unable to mark as completed." }
+      else
+        format.html { redirect_to tasks_path, alert: 'Unable to mark as completed.' }
         format.json { head :unprocessable_entity }
-    end
+      end
     end
   end
-
 
   def destroy
     @task = Task.find(params[:id])
     if @task.destroy
       respond_to do |format|
         format.js
-        format.html { redirect_to tasks_path, notice: "Task was successfully deleted."  }
+        format.html { redirect_to tasks_path, notice: 'Task was successfully deleted.' }
       end
     end
   end
 
   private
-    def set_task
-      @task = Task.find(params.expect(:id))
-    end
 
-    def task_params
-      params.expect(task: [ :title, :description, :completed, :due_date ])
-    end
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description, :completed, :due_date)
+  end
 end
